250d772e7e442569e755d50639e7e3eb8d174dd1
97190 <e.mayoute44@gmail.com>
2022-07-07 17:34:58 +0200

    Ajout controller et modele traiteur

---
+++ database/migrations/2022_07_07_142725_create_traiteurs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('traiteurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameEntreprise');
+            $table->string('phoneNumber')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('traiteurs');
+    }
+};
